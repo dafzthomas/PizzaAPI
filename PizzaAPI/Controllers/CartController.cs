e98b0e9f5f9814999d6549ed19e1e7b558c589b5
Dafydd Thomas <dafz@dafzthomas.com>
2017-02-11 17:53:42 +0000

    Added PizzaAPI project

---
+++ PizzaAPI/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using System.Web.Http;
+namespace PizzaAPI
+{
+	public class CartController : ApiController
+	{
+
+		[HttpGet]
+		public IHttpActionResult Index()
+		{
+			dynamic obj = new ExpandoObject();
+
+			obj.type = "message";
+			obj.data = new List<string>();
+
+			obj.data.Add("Hello");
+			obj.data.Add("world");
+			obj.data.Add("guys!");
+
+			return Ok(obj);
+		}
+	}
+}
